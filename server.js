a20732570884aeda225d1d6d4c3f10a209a8ea8c
MaxySpark <maxyspark@gmail.com>
2016-07-18 18:44:47 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app =  express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 // app.get('/',function (req,res) {
 //     res.send('Hello Express!!!!!!!!!!');
 // });
